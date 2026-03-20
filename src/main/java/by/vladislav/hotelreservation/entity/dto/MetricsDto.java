5dc0d97fb6c3b4c90ee1c042dc17c2cd30ab3957
Vlad <vladislavofficial.812@gmail.com>
2026-03-20 10:12:25 +0300

    feature: add frontend

--- src/main/java/by/vladislav/hotelreservation/entity/dto/MetricsDto.java
+++
@@ -1,9 +0,0 @@
-package by.vladislav.hotelreservation.entity.dto;
-
-public record MetricsDto(
-    long totalProcessed,
-    long totalProcessedUnsafe,
-    long activeTasksCount,
-    long activeTasksCountUnsafe,
-    long timestamp) {
-}
